84a8b6e5f24de383a71d380c5b908eac553b83f6
Nate Williams <nate@yogotech.com>
2022-12-28 15:56:48 -0700

    Change the index method return type from a String to a ResponseEntity

--- src/main/java/edu/carroll/cs389/HelloController.java
+++ src/main/java/edu/carroll/cs389/HelloController.java
@@ -1,12 +1,14 @@
 package edu.carroll.cs389;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String index() {
-        return "Hello from Spring Boot!";
+    public ResponseEntity<String> index() {
+        return new ResponseEntity<>("Hello from Spring Boot!", HttpStatus.OK);
     }
 }
