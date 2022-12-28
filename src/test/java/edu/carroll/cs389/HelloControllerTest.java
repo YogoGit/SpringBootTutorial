ad5c3d072c101639e63e4db56b0a616f676cedd2
Nate Williams <nate@yogotech.com>
2022-12-28 16:18:18 -0700

    Add unit test for new /user context in HelloController

--- src/test/java/edu/carroll/cs389/HelloControllerTest.java
+++ src/test/java/edu/carroll/cs389/HelloControllerTest.java
@@ -1,6 +1,8 @@
 package edu.carroll.cs389;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,5 +23,14 @@ public class HelloControllerTest {
         mockMvc.perform(get("/")).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Hello from Spring Boot!")));
+    }
+
+    @Test
+    public void userTest() throws Exception {
+        final String name = "Student";
+        mockMvc.perform(get("/user").param("name", name)).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(startsWith("Hello")))
+                .andExpect(content().string(endsWith(name + "!")));
     }
 }
