0f262c6ddf2f3223805c941a70c36463f3735d6c
Nate Williams <nate@yogotech.com>
2023-01-04 14:10:08 -0700

    Display the 'logged-in' username if we successfully login.

--- src/main/java/edu/carroll/cs341/web/controller/LoginController.java
+++ src/main/java/edu/carroll/cs341/web/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
@@ -18,16 +19,17 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginPost(@Valid @ModelAttribute LoginForm loginForm, BindingResult result) {
-        System.out.println("User '" + loginForm.getUsername() + "' attempted login");
+    public String loginPost(@Valid @ModelAttribute LoginForm loginForm, BindingResult result, RedirectAttributes attrs) {
         if (result.hasErrors()) {
             return "login";
         }
+        attrs.addAttribute("username", loginForm.getUsername());
         return "redirect:/loginSuccess";
     }
 
     @GetMapping("/loginSuccess")
-    public String loginSuccess() {
+    public String loginSuccess(String username, Model model) {
+        model.addAttribute("username", username);
         return "loginSuccess";
     }
 
