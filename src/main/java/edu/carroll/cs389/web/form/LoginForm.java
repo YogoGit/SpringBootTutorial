34ce7c667eda3bba9836d24411c9c1711d236e71
Nate Williams <nate@yogotech.com>
2023-01-06 11:35:34 -0700

    Add convenience constructors and methods

--- src/main/java/edu/carroll/cs389/web/form/LoginForm.java
+++ src/main/java/edu/carroll/cs389/web/form/LoginForm.java
@@ -12,6 +12,14 @@ public class LoginForm {
     @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
+    public LoginForm() {
+    }
+
+    public LoginForm(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
