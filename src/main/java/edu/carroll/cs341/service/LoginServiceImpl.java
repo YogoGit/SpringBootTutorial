11d70d5c521cba6cf5a1746b13b61e71f1e2add7
Nate Williams <nate@yogotech.com>
2023-03-24 08:20:16 -0600

    Update the LoginService to not use the web form - Instead, use two strings to avoid layering issues where the lower   level service relies on the calling layer's functionality.
    Tag: login_layering

--- src/main/java/edu/carroll/cs341/service/LoginServiceImpl.java
+++ src/main/java/edu/carroll/cs341/service/LoginServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import edu.carroll.cs341.jpa.model.Login;
 import edu.carroll.cs341.jpa.repo.LoginRepository;
-import edu.carroll.cs341.web.form.LoginForm;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,13 +17,14 @@ public class LoginServiceImpl implements LoginService {
     /**
      * Given a loginForm, determine if the information provided is valid, and the user exists in the system.
      *
-     * @param loginForm - Data containing user login information, such as username and password.
+     * @param username - Username of the person attempting to login
+     * @param password - Raw password provided by the user logging in
      * @return true if data exists and matches what's on record, false otherwise
      */
     @Override
-    public boolean validateUser(LoginForm loginForm) {
+    public boolean validateUser(String username, String password) {
         // Always do the lookup in a case-insensitive manner (lower-casing the data).
-        List<Login> users = loginRepo.findByUsernameIgnoreCase(loginForm.getUsername());
+        List<Login> users = loginRepo.findByUsernameIgnoreCase(username);
 
         // We expect 0 or 1, so if we get more than 1, bail out as this is an error we don't deal with properly.
         if (users.size() != 1)
@@ -32,7 +32,7 @@ public class LoginServiceImpl implements LoginService {
         Login u = users.getFirst();
         // XXX - Using Java's hashCode is wrong on SO many levels, but is good enough for demonstration purposes.
         // NEVER EVER do this in production code!
-        final String userProvidedHash = Integer.toString(loginForm.getPassword().hashCode());
+        final String userProvidedHash = Integer.toString(password.hashCode());
         if (!u.getHashedPassword().equals(userProvidedHash))
             return false;
 
